70902ebeb3adb4faf23044ddb602b537208d31b8
nefario19 <taifrahim@tutanota.com>
2022-03-03 17:58:42 +0100

    Created a landing page with a navigation bar and a appbar. The buttons don't route to other pages yet.

--- bushido_app/lib/main.dart
+++ bushido_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() => runApp(const BushidoApp());
 
@@ -7,6 +8,62 @@ class BushidoApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp(
+        title: 'Bushido App',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          primaryColor: const Color(0xFF0A0E21),
+          scaffoldBackgroundColor: const Color(0xFF0A0E21),
+          appBarTheme: const AppBarTheme(
+            color: Colors.transparent,
+            elevation: 0,
+          ),
+        ),
+        home: const LandingPage());
+  }
+}
+
+class LandingPage extends StatefulWidget {
+  const LandingPage({Key? key}) : super(key: key);
+
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text('Landing Page'),
+        ),
+      ),
+      body: const Center(
+        child: Text('Landing Page'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(FontAwesomeIcons.newspaper), label: 'News'),
+          BottomNavigationBarItem(
+              icon: Icon(FontAwesomeIcons.calendar), label: 'Activities'),
+          BottomNavigationBarItem(
+              icon: Icon(FontAwesomeIcons.user), label: 'Members'),
+        ],
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[500],
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
---
+++ bushido_app/lib/screens/members_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MembersPage extends StatelessWidget {
+  const MembersPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('Members Page'),
+      ),
+    );
+  }
+}
