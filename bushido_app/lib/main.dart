ee5731ab71c64695a3bfcde1dd82e4371bb45b6b
nefario19 <taifrahim@tutanota.com>
2022-03-04 09:48:55 +0100

    WIP: Created a class for the landing page so the main.dart doesn't look so overwhelming.

--- bushido_app/lib/main.dart
+++ bushido_app/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
+import 'screens/landing_page.dart';
 void main() => runApp(const BushidoApp());
 
 class BushidoApp extends StatelessWidget {
@@ -20,50 +19,5 @@ class BushidoApp extends StatelessWidget {
           ),
         ),
         home: const LandingPage());
-  }
-}
-
-class LandingPage extends StatefulWidget {
-  const LandingPage({Key? key}) : super(key: key);
-
-  @override
-  State<LandingPage> createState() => _LandingPageState();
-}
-
-class _LandingPageState extends State<LandingPage> {
-  int _selectedIndex = 0;
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Center(
-          child: Text('Landing Page'),
-        ),
-      ),
-      body: const Center(
-        child: Text('Landing Page'),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-              icon: Icon(FontAwesomeIcons.newspaper), label: 'News'),
-          BottomNavigationBarItem(
-              icon: Icon(FontAwesomeIcons.calendar), label: 'Activities'),
-          BottomNavigationBarItem(
-              icon: Icon(FontAwesomeIcons.user), label: 'Members'),
-        ],
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[500],
-        onTap: _onItemTapped,
-      ),
-    );
   }
 }
--- bushido_app/lib/screens/landing_page.dart
+++ bushido_app/lib/screens/landing_page.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class News extends StatelessWidget {
-  const News({Key? key}) : super(key: key);
+class LandingPage extends StatefulWidget {
+  const LandingPage({Key? key}) : super(key: key);
+
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text('Landing Page'),
+        ),
+      ),
+      body: const Center(
+        child: Text('Landing Page'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(FontAwesomeIcons.newspaper), label: 'News'),
+          BottomNavigationBarItem(
+              icon: Icon(FontAwesomeIcons.calendar), label: 'Activities'),
+          BottomNavigationBarItem(
+              icon: Icon(FontAwesomeIcons.user), label: 'Members'),
+        ],
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[500],
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
