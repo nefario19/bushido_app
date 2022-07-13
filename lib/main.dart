0e6d76379a4c8e8a949d27ae800f56f3a792e76a
nefario19 <taifrahim@gmail.com>
2022-07-13 12:04:56 +0200

    In the main dart file I've exposed the provider as a global

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:bushido_app/constants.dart';
 import 'package:bushido_app/data/news_item_state_notifier.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'screens/landing_page.dart';
 
 void main() => runApp(const BushidoApp());
 
-
+final newsItemProvider =
+    StateNotifierProvider<NewsItemStateNotifier, NewsItemState>((ref) => NewsItemStateNotifier());
 
 class BushidoApp extends StatelessWidget {
   const BushidoApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
         title: 'Bushido App',
         debugShowCheckedModeBanner: true,
